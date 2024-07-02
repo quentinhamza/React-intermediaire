5f62dd02a62ecde8666e54a1895eb6b5a19b481a
Quentin Hamza <quentinhamzaent@gmail.com>
2024-07-02 14:45:03 +0200

    ss

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './pages/Home'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/';
+import Survey from './pages/Survey/';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
