8afb3fe24be013cc6396eb45b448230cad880e8c
Quentin Hamza <quentinhamzaent@gmail.com>
2024-07-06 21:15:50 +0200

    ok

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,4 +1,5 @@
 import { formatJobList } from './'
+import { formatQueryParams } from './'
 
 describe('The formatJobList function', () => {
   it('should add a comma to a word', () => {
@@ -8,5 +9,19 @@ describe('The formatJobList function', () => {
   it('should not add a comma to the last element of the list', () => {
     const expectedState = 'item3'
     expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
+  })
+})
+
+describe('formatQueryParams', () => {
+  test('should format query params correctly with one answer', () => {
+    const answers = { 1: true }
+    const result = formatQueryParams(answers)
+    expect(result).toBe('a1=true')
+  })
+
+  test('should format query params correctly with multiple answers', () => {
+    const answers = { 1: true, 2: false, 3: true }
+    const result = formatQueryParams(answers)
+    expect(result).toBe('a1=true&a2=false&a3=true')
   })
 })
